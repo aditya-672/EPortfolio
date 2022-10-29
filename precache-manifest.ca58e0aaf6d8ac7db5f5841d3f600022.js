51b1749f2e90ccb0f223fcae88c42f6a931a3473
aditya-672 <waghmareaditya08@gmail.com>
2022-10-29 11:13:58 +0530

    Updates

---
+++ precache-manifest.ca58e0aaf6d8ac7db5f5841d3f600022.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74879bac65efd4573fac4f8f8047e630",
+    "url": "/EPortfolio/index.html"
+  },
+  {
+    "revision": "0d1d7c51fb6b9a2aebee",
+    "url": "/EPortfolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "5cc3901ce182161d3b12",
+    "url": "/EPortfolio/static/js/2.318b7d2b.chunk.js"
+  },
+  {
+    "revision": "0d1d7c51fb6b9a2aebee",
+    "url": "/EPortfolio/static/js/main.b95f4150.chunk.js"
+  },
+  {
+    "revision": "58483bc5bef70a9c6a61",
+    "url": "/EPortfolio/static/js/runtime~main.2475efd2.js"
+  },
+  {
+    "revision": "fc14ddab5c6d9c1398603295df42f691",
+    "url": "/EPortfolio/static/media/im1.fc14ddab.jpg"
+  },
+  {
+    "revision": "d416a12aa87964a6af0cd3f9ef4ab20c",
+    "url": "/EPortfolio/static/media/im2.d416a12a.jpg"
+  },
+  {
+    "revision": "5fd578224e64eec070dceee64058632d",
+    "url": "/EPortfolio/static/media/im3.5fd57822.jpg"
+  },
+  {
+    "revision": "a9aeef0d4f0ecc6684af1f48b81d2fac",
+    "url": "/EPortfolio/static/media/profile.a9aeef0d.jpg"
+  }
+]);
